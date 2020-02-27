f5dcce0b05759a29f217d1150deae173ac36b1af
Max Pulyk <43215127+Maxgioman@users.noreply.github.com>
2020-02-27 21:21:33 +0200

    Update devs-list.js
    Max just updated this

--- src/devs-list.js
+++ src/devs-list.js
@@ -1,5 +1,5 @@
 const mentor = 'Kostya';
-const dev1 = 'put your name here';
+const dev1 = 'Max';
 const dev2 = 'put your name here';
 const dev3 = 'put your name here';
 const dev4 = 'put your name here';
