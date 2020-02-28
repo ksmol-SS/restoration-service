cfec15e19f9fb6b9d4a71709ed17f195620f7397
Oleksiy <loxaskorost@gmail.com>
2020-02-28 23:22:08 +0200

    First Commit

--- src/devs-list.js
+++ src/devs-list.js
@@ -1,6 +1,6 @@
 const mentor = 'Kostya';
 const dev1 = 'Max';
-const dev2 = 'put your name here';
+const dev2 = 'Loxa';
 const dev3 = 'put your name here';
 const dev4 = 'put your name here';
 const dev5 = 'put your name here';
