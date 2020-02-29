ba34554dd05fa5edb01b14f626c6a1545a980d3c
Maxgioman <maximchuk2001@gmail.com>
2020-02-29 17:00:03 +0200

    Update devs-list.js
    Tried to use ssh, but GithubDesktop is hundred times better. Also added a honorific for myself and Loxa

--- src/devs-list.js
+++ src/devs-list.js
@@ -1,6 +1,6 @@
 const mentor = 'Kostya';
-const dev1 = 'Max';
-const dev2 = 'Loxa';
+const dev1 = 'Max, who used console';
+const dev2 = 'Loxa the Lead';
 const dev3 = 'put your name here';
 const dev4 = 'put your name here';
 const dev5 = 'put your name here';
