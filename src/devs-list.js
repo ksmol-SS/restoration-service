94bda7d22a84e55e838de3b6497122d0931f3b55
Maxgioman <maximchuk2001@gmail.com>
2020-03-10 15:01:54 +0200

    Update devs-list.js

--- src/devs-list.js
+++ src/devs-list.js
@@ -1,6 +1,6 @@
 const mentor = 'Kostya';
 const dev1 = 'DJ Max';
-const dev2 = 'Loxa';
+const dev2 = 'Loxa the Lead';
 const dev3 = 'Adiok';
 const dev4 = 'put your name here';
 const dev5 = 'put your name here';
