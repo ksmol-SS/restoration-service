b65fa48be3261ee9c83336c58e91579460594358
Oleksiy <loxaskorost@gmail.com>
2020-03-13 19:49:46 +0200

    mycommit

--- src/devs-list.js
+++ src/devs-list.js
@@ -2,5 +2,5 @@ const mentor = 'Kostya';
 const dev1 = 'DJ Max';
 const dev2 = 'Mr Loxa';
 const dev3 = 'Unknown Adiok';
-const dev4 = 'Dev Misha';
+const dev4 = 'Misha';
 const dev5 = 'put your name here';
